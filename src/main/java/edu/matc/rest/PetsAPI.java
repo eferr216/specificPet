4cd69db99d40ceb97eda987792718c07f4c902be
Enrique <eferreira@madisoncollege.edu>
2024-12-08 07:10:15 -0600

    Week 9 complete.

--- src/main/java/edu/matc/rest/PetsAPI.java
+++ src/main/java/edu/matc/rest/PetsAPI.java
@@ -3,9 +3,7 @@ package edu.matc.rest;
 import edu.matc.entity.Pet;
 import edu.matc.persistence.GenericDao;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +12,11 @@ import java.util.List;
 public class PetsAPI {
     // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/html"
+    // The Java method will produce content identified by the MIME Media type "application/json"
     @Produces("application/json")
     public Response getMesssage() {
         GenericDao petDao = new GenericDao(Pet.class);
+
         String output = "[";
 
         List<Pet> pets = petDao.getAll();
@@ -35,6 +34,29 @@ public class PetsAPI {
         if (output.length() > 2) {
             output = output.substring(0, output.length() - 2);  // Remove the last 2 characters
         }
+
+        output += "]";
+
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    // The Java method will produce content identified by the MIME Media type "text/html"
+    @Path("/{id}")
+    @Produces("application/json")
+    public Response getMessageWithCoordinateParams(@PathParam("id") int x) {
+        GenericDao petDao = new GenericDao(Pet.class);
+
+        String output = "[";
+
+        Pet pet = (Pet) petDao.getById(x);
+
+        output += "{\"Pet Id\": " + pet.getId()
+                + ", \"Pet Age\": " + pet.getPetAge()
+                + ", \"Pet Weight\": " + pet.getPetWeight()
+                + ", \"Pet Species\": \"" + pet.getPetSpecies()
+                + "\", \"Pet Color\": \"" + pet.getPetColor()
+                + "\"}";
 
         output += "]";
 
